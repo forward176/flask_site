0ae60f5210cc4e56e19acf15b0745afb8474298d
forward176 <onlydiscordfresko@mail.ru>
2024-10-10 11:47:14 +0300

    add registration

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')

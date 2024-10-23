af02979326b9ea76c10b28626ec2ebb5e5987fb4
forward176 <onlydiscordfresko@mail.ru>
2024-10-23 11:34:14 +0300

    registration

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ def index():
         return render_template('index.html')
     
 
-@app.route('/registration')
+@app.route('/registration', methods=['GET', 'POST'])
 def registration():
+    if request.method == 'POST':
+        login = request.form.get('email')
+        password = request.form.get('password') 
+        # проверяем существует ли пользователь и создаём только если не существует DB.is_user_exist
+        DB.create_user(login, password)
+        # нужно добавить под формой вывод: успешная регистрация или пользователь с таким email уже зарегистрирован
     return render_template('registration.html')
+    

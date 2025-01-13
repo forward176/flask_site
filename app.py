def7e236537b1a941b0f932179a76c7803a81b6b
forward176 <onlydiscordfresko@mail.ru>
2025-01-13 22:41:09 +0300

    control available levels

--- app.py
+++ app.py
@@ -62,11 +62,8 @@ def levels():
 def level():
     if 'login' not in session: 
         return index()  #  TODO СДЕЛАТЬ НОРМАЛЬНЫЙ РЕДИРЕКТ НА ГЛАВНУЮ
-    
 
-
-    if request.method == 'POST':  ## TODO СЮДА НЕ ПОПАЛИ
-        print('!!!!!!!!!!!!!!!!!!')
+    if request.method == 'POST': 
         try:
             data = request.json
             if 'update_count_level' in data:
@@ -77,43 +74,52 @@ def level():
         except (KeyError, json.JSONDecodeError) as e:
             return jsonify({'error': 'Invalid data format'}), 400  
 
-
-
-    context = {}
-    if 'lvl' in request.args:
-        requested_lvl = int(request.args.get('lvl'))
-        max_available_lvl = DB.get_count_level(session['login']) + 1
-        if requested_lvl > max_available_lvl or requested_lvl > COUNT_GAME_LVLS or requested_lvl % 2 == 0:
-            return redirect(url_for('levels'), 301)
-        context['lvl'] = requested_lvl
-        path = f'levels/{(context["lvl"] + 1) // 2}.lvl'
-        mat = open(path, encoding="UTF-8").read().split()
-        context["mat"] = mat
-        context["is_max_available_lvl"] = requested_lvl == max_available_lvl
-        return render_template('level.html', context=context)    
-    else:
+    if 'lvl' not in request.args:
         return redirect(url_for('levels'), 301)
+    
+    context = {}
+    requested_lvl = int(request.args.get('lvl'))
+    max_available_lvl = DB.get_count_level(session['login']) + 1
+    if requested_lvl > max_available_lvl or requested_lvl > COUNT_GAME_LVLS or requested_lvl % 2 == 0:
+        return redirect(url_for('levels'), 301)
+    context['lvl'] = requested_lvl
+    path = f'levels/{(context["lvl"] + 1) // 2}.lvl'
+    mat = open(path, encoding="UTF-8").read().split()
+    context["mat"] = mat
+    context["is_max_available_lvl"] = requested_lvl == max_available_lvl
+    return render_template('level.html', context=context)    
 
 
 @app.route('/question', methods=['GET', 'POST'])
 def question():
     if 'login' not in session: 
         return index()  #  TODO СДЕЛАТЬ НОРМАЛЬНЫЙ РЕДИРЕКТ НА ГЛАВНУЮ
-    context = {}
-    if 'lvl' in request.args:
-        context['lvl'] = int(request.args.get('lvl'))
-        with open('questions.txt','r',encoding='UTF-8') as file:
-            sp = []
-            lines = file.readlines()
-            # print(len(lines))
-            for i in range(0, len(lines) - 1, 2):
-                quest = lines[i].strip()
-                ans = lines[i + 1].strip()
-                sp.append((quest, ans))
-        # print(*sp, sep='\n')
-        context['question'] = sp[context['lvl']][0]
-        context['answer'] = sp[context['lvl']][1]
-    else:
+    
+    if request.method == 'POST':  
+        try:
+            data = request.json
+            if 'update_count_level' in data:
+                DB.update_count_level(session['login'])                            
+                return jsonify({'message': 'Success!'}), 200
+            else:
+                raise KeyError('Value key not found')
+        except (KeyError, json.JSONDecodeError) as e:
+            return jsonify({'error': 'Invalid data format'}), 400  
+
+    if 'lvl' not in request.args:
         return redirect(url_for('levels'), 301)
-    print(context)
+    
+    context = {}
+    requested_lvl = int(request.args.get('lvl'))    
+    max_available_lvl = DB.get_count_level(session['login']) + 1
+    if requested_lvl > max_available_lvl or requested_lvl > COUNT_GAME_LVLS or requested_lvl % 2 == 1:
+        print('!!!')
+        return redirect(url_for('levels'), 301)   
+    question_number = requested_lvl // 2 - 1 
+    with open('questions.txt','r',encoding='UTF-8') as file:
+        lines = file.readlines()
+    context['question'] = lines[question_number * 2].strip()
+    context['answer'] = lines[question_number * 2 + 1].strip()
+    context['lvl'] = requested_lvl
+    context["is_max_available_lvl"] = requested_lvl == max_available_lvl   
     return render_template('question.html', context=context)
